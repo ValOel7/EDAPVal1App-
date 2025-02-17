213443dec48505f47d33e8cc41ca29b1ebe47a3b
Valusha Oelofse <142802666+ValOel7@users.noreply.github.com>
2025-02-17 17:08:28 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 
+st.title("This is my EDAB App")
 # Generate random time series data
 if st.button("Test this"):
   time_series = np.random.randn(100)
-
+st.subheader("This is the time series graph")
   # Plot the time series
   plt.plot(time_series)
   plt.title("Random 100-Unit Time Series")
   plt.xlabel("Time")
   plt.ylabel("Value")
-  plt.show()
+  st.pyplot(plt.gfc())
